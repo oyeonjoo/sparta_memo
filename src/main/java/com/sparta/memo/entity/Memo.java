0e4f8621e76b8856d8b8bb8d3a1fefab9b6913f6
oyeonjoo <oyj0842@naver.com>
2024-01-22 06:19:10 +0900

    .

--- src/main/java/com/sparta/memo/entity/Memo.java
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -17,9 +17,4 @@ public class Memo {
         this.username = requestDto.getUsername();
         this.contents = requestDto.getContents();
     }
-
-    public void update(MemoRequestDto requestDto) {
-        this.username = requestDto.getUsername();
-        this.contents = requestDto.getContents();
-    }
 }
